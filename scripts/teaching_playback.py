e5e482ba471394d5e41587337a082a115c947def
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2023-07-27 21:54:32 +0900

    add save fuction

--- scripts/teaching_playback.py
+++ scripts/teaching_playback.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python
 
-from ast import mod
-import keyboard
 import rospy
 import moveit_commander
 import json
 import os
-from tf.transformations import quaternion_from_euler
+import math
+import time
 
 class TeachingPlayback:
     def __init__(self):
@@ -22,16 +21,17 @@ class TeachingPlayback:
         self.arm.go()
 
         self.pose = self.arm.get_current_pose()
-        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)
-        self.pose.pose.orientation.x = q[0]
-        self.pose.pose.orientation.y = q[1]
-        self.pose.pose.orientation.z = q[2]
-        self.pose.pose.orientation.w = q[3]
+        self.pose.pose.orientation.x = -math.sqrt(0.5)
+        self.pose.pose.orientation.y = math.sqrt(0.5)
+        self.pose.pose.orientation.z = 0
+        self.pose.pose.orientation.w = 0
         self.last_modified = None
         self.hand = self.gripper.get_current_joint_values()
+        self.positions = []
+        self.hands = []
     
     def update(self, event):
-        command = [0,0,0,0]
+        command = [0,0,0,0,0,0]
         try:
             if os.path.exists('key_command.json'):
                 modified = os.path.getmtime('key_command.json')
@@ -43,20 +43,46 @@ class TeachingPlayback:
         except Exception as e:
             print("Error occured: {e}")
 
-        print(command)
-        self.pose.pose.position.x += command[0]*0.01
-        self.pose.pose.position.y += command[1]*0.01
-        self.pose.pose.position.z += command[2]*0.01
-        self.arm.set_pose_target(self.pose)
-        self.arm.go()
-        self.hand[0] += command[3]*0.1
-        self.hand[1] += command[3]*0.1
-        self.gripper.set_joint_value_target(self.hand)
-        self.gripper.go()
+        #print(command)
+        if command[0] != 0 or command[1] != 0 or command[2] != 0:
+            self.pose.pose.position.x += command[0]*0.01
+            self.pose.pose.position.y += command[1]*0.01
+            self.pose.pose.position.z += command[2]*0.01
+            self.arm.set_pose_target(self.pose)
+            self.arm.go()
+        
+        if command[3] != 0:
+            self.hand[0] += command[3]*0.1
+            self.hand[1] += command[3]*0.1
+            self.gripper.set_joint_value_target(self.hand)
+            self.gripper.go()
+        
+        if command[4] == 1:
+            self.positions.append(self.arm.get_current_pose())
+            self.hands.append(self.gripper.get_current_joint_values())
+            print("SAVE: "+str(len(self.positions)))
+        elif command[4] == -1:
+            self.positions.pop()
+            self.hands.pop()
+            self.pose = self.positions[-1]
+            self.hand = self.hands[-1]
+            self.arm.set_pose_target(self.pose)
+            self.arm.go()
+            self.gripper.set_joint_value_target(self.hand)
+            self.gripper.go()
+            print("DELETE: "+str(len(self.positions)))
+        elif command[5] == 1:
+            for position, hand in zip(self.positions, self.hands):
+                print(position)
+                self.arm.set_pose_target(position)
+                self.arm.go()
+                self.gripper.set_joint_value_target(hand)
+                self.gripper.go()
+            self.pose = self.positions[-1]
+            self.hand = self.hands[-1]
 
 if __name__ == '__main__':
     rospy.init_node("crane_x7_teaching_playback")
     teaching_playback = TeachingPlayback()
     timer = rospy.Timer(rospy.Duration(0.1), teaching_playback.update)
     rospy.spin()
-
