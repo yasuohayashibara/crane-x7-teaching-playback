5db90f08e40736dd7ea0da43a5cff6af0219c888
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2025-07-26 17:29:41 +0900

    modify keyboard2command.py

--- scripts/keyboard2command.py
+++ scripts/keyboard2command.py
@@ -2,6 +2,10 @@ import keyboard
 import json
 import time
 
+# 一度だけ反応させたいキーをセットで定義
+once_keys = {'w', 'q', 'r', 'p'}
+
+# [key, [command], is_pressed]
 commands = [
     ['1', [1,-1,0,0,0,0], False],
     ['2', [1,0,0,0,0,0], False],
@@ -24,17 +28,26 @@ commands = [
 def on_press(key):
     for i in range(len(commands)):
         if key.name == commands[i][0]:
-            commands[i][2] = True
+            if key.name in once_keys:
+                # 一度だけ True にする（押しっぱなしは無視）
+                if not commands[i][2]:
+                    commands[i][2] = True
+            else:
+                commands[i][2] = True
 
 def on_release(key):
-    for i in range(len(commands)-3):
+    for i in range(len(commands)):
         if key.name == commands[i][0]:
-            commands[i][2] = False
+            if key.name not in once_keys:
+                # 通常のキーは押しっぱなしを解除
+                commands[i][2] = False
 
+# 登録
 for key, _, _ in commands:
     keyboard.on_press_key(key, on_press)
     keyboard.on_release_key(key, on_release)
 
+# メインループ
 while True:
     sum_command = [0, 0, 0, 0, 0, 0]
     for _, command, is_pressed in commands:
@@ -42,7 +55,11 @@ while True:
             sum_command = [x + y for x , y in zip(sum_command, command)]
     with open('key_command.json', 'w') as f:
         json.dump({'command': sum_command}, f)
-    for i in range(len(commands)-3,len(commands)):
-        commands[i][2] = False
     print(sum_command)
+
+    # 一度だけ反応させるキーは自動でフラグを False に戻す
+    for i in range(len(commands)):
+        if commands[i][0] in once_keys:
+            commands[i][2] = False
+
     time.sleep(0.1)
